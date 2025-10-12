cf4072fc79dce824736b4730e1e455394d0fa7b7
roman <romanpz051@gmail.com>
2025-10-12 16:12:00 +0200

    0.0.0.3 -Generate the frontend and the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {
   NgbAccordionBody, NgbAccordionButton, NgbAccordionCollapse,
   NgbAccordionDirective,
   NgbAccordionHeader,
   NgbAccordionItem
 } from '@ng-bootstrap/ng-bootstrap';
+import {FaIconComponent, FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import {fontAwesomeIcons} from './shared/font-awesome-icons';
 
 @Component({
-  selector: 'app-root',
+  selector: 'wac-root',
   standalone: true,
-  imports: [RouterOutlet, NgbAccordionDirective, NgbAccordionItem, NgbAccordionHeader, NgbAccordionBody, NgbAccordionCollapse, NgbAccordionButton],
+  imports: [RouterOutlet, NgbAccordionDirective, NgbAccordionItem, NgbAccordionHeader, NgbAccordionBody, NgbAccordionCollapse, NgbAccordionButton, FaIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'whatsapp-clone-front';
+
+  private faIconLibrary = inject(FaIconLibrary)
+
+  ngOnInit(): void {
+    this.initFontAwesome()
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons)
+  }
 }
