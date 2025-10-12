b8ad1b4cedd29910aea64f38d2ec6c75773dd9bc
roman <romanpz051@gmail.com>
2025-10-12 15:47:38 +0200

    0.0.0.2 -Generate the frontend and the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  NgbAccordionBody, NgbAccordionButton, NgbAccordionCollapse,
+  NgbAccordionDirective,
+  NgbAccordionHeader,
+  NgbAccordionItem
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbAccordionDirective, NgbAccordionItem, NgbAccordionHeader, NgbAccordionBody, NgbAccordionCollapse, NgbAccordionButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
