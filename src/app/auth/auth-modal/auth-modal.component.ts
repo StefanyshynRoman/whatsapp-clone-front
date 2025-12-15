c1388f98fd6e597873a1caa734d96520e00d0f2c
roman <romanpz051@gmail.com>
2025-12-15 09:57:13 +0100

    0.0.0.6 -Authorization_keycloak

---
+++ src/app/auth/auth-modal/auth-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wac-auth-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-modal.component.html',
+  styleUrl: './auth-modal.component.scss'
+})
+export class AuthModalComponent {
+
+}
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  keycloak: {
+    url: "http://localhost:9081",
+    realm: "whatsapp-clone",
+    clientId: "whatsapp-clone"
+  },
+  API_URL: "http://localhost:4200/api",
+};
