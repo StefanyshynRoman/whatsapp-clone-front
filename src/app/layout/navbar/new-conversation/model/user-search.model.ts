805171bf0b4fb9911935cf7d30442870fb49cd06
roman <romanpz051@gmail.com>
2026-01-11 13:06:20 +0100

    0.0.1.0 -Create conversations

---
+++ src/app/layout/navbar/new-conversation/model/user-search.model.ts
@@ -0,0 +1,6 @@
+import {Pagination} from '../../../../shared/model/state.model';
+
+export interface SearchQuery{
+  query:string,
+  page: Pagination
+}
---
+++ src/app/layout/navbar/new-conversation/new-conversation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wac-new-conversation',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-conversation.component.html',
+  styleUrl: './new-conversation.component.scss'
+})
+export class NewConversationComponent {
+
+}
