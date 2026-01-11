c16e4f255c330371d9bc022f1b70880fcadae7fe
roman <romanpz051@gmail.com>
2026-01-11 14:08:15 +0100

    0.0.1.1 - toastservice

--- src/app/layout/navbar/new-conversation/new-conversation.component.ts
+++ src/app/layout/navbar/new-conversation/new-conversation.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {UserSearchService} from '../user-search.service';
+import {NgbActiveOffcanvas} from '@ng-bootstrap/ng-bootstrap';
+import {ToastService} from '../../../shared/toast/toast.service';
+import {BaseUser} from '../../../shared/model/user.model';
+import {Pagination} from '../../../shared/model/state.model';
+import {Subscription} from 'rxjs';
+import {SearchQuery} from './model/user-search.model';
 
 @Component({
   selector: 'wac-new-conversation',
@@ -7,6 +14,67 @@ import { Component } from '@angular/core';
   templateUrl: './new-conversation.component.html',
   styleUrl: './new-conversation.component.scss'
 })
-export class NewConversationComponent {
+export class NewConversationComponent implements OnInit, OnDestroy {
+
+  searchService = inject(UserSearchService);
+  toastService = inject(ToastService);
+  activeOffCanvas = inject(NgbActiveOffcanvas);
+
+  public query: string = "";
+  public usersResults = new Array<BaseUser>();
+
+  searchPage: Pagination = {
+    page: 0,
+    size: 20,
+    sort: ["firstName, ASC"]
+  }
+  loadingSearch = true;
+  searchSubscription: Subscription | undefined;
+
+
+  ngOnDestroy(): void {
+    if (this.searchSubscription) {
+      this.searchSubscription.unsubscribe();
+    }
+  }
+
+  ngOnInit(): void {
+    this.initSearchResultListener();
+  }
+
+  initSearchResultListener(): void {
+    this.searchSubscription = this.searchService.searchResult
+      .subscribe(usersState => {
+        if (usersState.status === "OK" && usersState.value) {
+          this.usersResults = usersState.value;
+        } else if (usersState.status === "ERROR") {
+          this.toastService.show("Error occured when fetching search result, please try again", "DANGER");
+        }
+        this.loadingSearch = false;
+      });
+    const searchQuery: SearchQuery = {
+      query: "",
+      page: this.searchPage
+    }
+    this.searchService.search(searchQuery);
+  }
+
+  onQueryChange(newQuery: string): void {
+    this.loadingSearch = true;
+    const searchQuery: SearchQuery = {
+      query: newQuery,
+      page: this.searchPage
+    }
+    this.searchService.search(searchQuery);
+  }
+
+  onCloseNav() {
+    this.activeOffCanvas.close();
+  }
+
+  handleConversation(userPublicId: string): void {
+   // todo this.conversationService.handleNavigateToConversation(userPublicId);
+    this.activeOffCanvas.close();
+  }
 
 }
